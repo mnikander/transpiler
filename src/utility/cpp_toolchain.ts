6020b7fffd5d42f926f10d9847a8cd4742281699
Marco Nikander <m4n1k.developer@gmail.com>
2025-01-29 22:40:44 +0100

    compile and execute generated C++ example

--- src/utility/cpp_toolchain.ts
+++ src/utility/cpp_toolchain.ts
@@ -1,11 +1,13 @@
 import { exec } from 'node:child_process'
 
-let includes: string = `#include <cmath>
+function includes(): string {
+    return `#include <cmath>
 #include <cstdlib>
 #include <iostream>
 #include <functional>
 
 `;
+}
 
 function main(body: string): string {
     return `int main() {
@@ -14,6 +16,26 @@ return EXIT_SUCCESS;
 }`;
 }
 
+function write(filename: string, content: string): string {
+    return "echo '" + includes() + main(content) + `' > out/${filename}.cpp`;
+}
+
+function compile(filename: string): string {
+    return `g++ -o out/${filename} out/${filename}.cpp`;
+}
+
+function execute(filename: string): string {
+    return `./out/${filename} > out/${filename}.txt`;
+}
+
+function clearfiles(filename: string): string {
+    return `echo '//empty file' > out/${filename}.cpp && echo '' > out/${filename}.txt`;
+}
+
+let filename: string = "hello";
 let content: string = 'std::cout << "Hello, world" << std::endl;';
-let foo: string = "echo '" + includes + main(content) + "' > out/output.cpp";
-exec(foo);
+
+exec(clearfiles(filename) + " && "
+    + write(filename, content) + " && "
+    + compile(filename) + " && "
+    + execute(filename));
