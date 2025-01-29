9a5ecffffb5b6ad2d5056bfbc6fb7ef351b789c3
Marco Nikander <m4n1k.developer@gmail.com>
2025-01-29 22:16:21 +0100

    generate rudimentary 'hello world' cpp file, without formatting

--- src/utility/cpp_toolchain.ts
+++ src/utility/cpp_toolchain.ts
@@ -1,3 +1,19 @@
 import { exec } from 'node:child_process'
 
-exec('echo "Hello, world." > out/output.txt');
+let includes: string = `#include <cmath>
+#include <cstdlib>
+#include <iostream>
+#include <functional>
+
+`;
+
+function main(body: string): string {
+    return `int main() {
+${body}
+return EXIT_SUCCESS;
+}`;
+}
+
+let content: string = 'std::cout << "Hello, world" << std::endl;';
+let foo: string = "echo '" + includes + main(content) + "' > out/output.cpp";
+exec(foo);
