b9f497fdc68d10bebe1a6bd731346ae14ed0c210
Marco Nikander <m4n1k.developer@gmail.com>
2025-01-29 18:51:32 +0100

    folder for integer tests and corresponding JSON file

---
+++ test/integer/integer.test.ts
@@ -0,0 +1,11 @@
+import { describe, it, expect } from 'vitest';
+import { generate_integer, Integer } from '../../src/integer';
+import data from './test_01.json';
+
+describe('Integer', () => {
+    const node = data as Integer;
+
+    it('string comparison', () => {
+        expect(generate_integer(node)).toBe("5");
+    });
+});
