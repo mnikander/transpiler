e672b7f619603a85c04811bfb4dce06ca7aff8c6
Marco Nikander <m4n1k.developer@gmail.com>
2025-01-30 01:14:38 +0100

    start testing C++ toolchain error handling

--- test/cpp_toolchain.test.ts
+++ test/cpp_toolchain.test.ts
@@ -3,6 +3,15 @@ import { cpp_toolchain } from '../src/utility/cpp_toolchain'
 
 describe('C++ Toolchain', () => {
 
+    // TODO: figure out why part of the error message still makes it through to the Vitest console
+    //       maybe it's printing some stuff to stdout before the process exits with the error
+    it('error message', () => {
+        let filename: string = "error";
+        let content: string = 'provoke_error; // an unknown variable name causes a compilation error';
+        const result: string = cpp_toolchain(filename, content).split('\n')[0];
+        expect(result).toBe("Error while executing C++ toolchain:");
+    });
+
     it('hello world', () => {
         let filename: string = "hello";
         let content: string = 'std::cout << "Hello, world" << std::endl;';
