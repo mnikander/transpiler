4dcd119a6ce38f8dbcd41224ee38144d8c6e9d66
Marco Nikander <m4n1k.developer@gmail.com>
2025-02-03 23:37:34 +0100

    uniform file names for test-case output

--- test/cpp_toolchain.test.ts
+++ test/cpp_toolchain.test.ts
@@ -8,14 +8,14 @@ describe('C++ Toolchain', () => {
     // TODO: figure out why part of the error message still makes it through to the Vitest console
     //       maybe it's printing some stuff to stdout before the process exits with the error
     it('error message', () => {
-        let filename: string = "error";
+        let filename: string = "test_cpp_error";
         let content: string = 'provoke_error; // use an unknown variable name to cause a compilation error';
         const result: string = cpp_toolchain(filename, content).split('\n')[0];
         expect(result).toBe("Error while executing C++ toolchain:");
     });
 
     it('hello world', () => {
-        let filename: string = "hello";
+        let filename: string = "test_cpp_hello";
         let content: string = 'std::cout << "Hello, world" << std::endl;';
         const result: string = cpp_toolchain(filename, content);
         expect(result).toBe("Hello, world\n");
