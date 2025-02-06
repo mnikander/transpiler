4830d8d9275475672232088e86029450709fdc02
Marco Nikander <m4n1k.developer@gmail.com>
2025-02-06 15:39:02 +0100

    fix missing newline and cleanup a function call

--- src/generate.ts
+++ src/generate.ts
@@ -80,7 +80,7 @@ function generate_define(ast: any): string {
         return `auto const ${generate(tail[0])};`;
     }
     else if (tail.length == 2) {
-        return `auto const ${generate(tail[0])} = ${generate(tail[1])};`;
+        return `auto const ${generate(tail[0])} = ${generate(tail[1])};\n`;
     } else {
         assert(false, `'define' requires 1 or 2 arguments, ${tail.length} provided <${tail.toString}>`);
         return " /* ERROR: INCORRECT NUMBER OF ARGUMENTS */ ";
@@ -120,7 +120,7 @@ function is_higher_order_function(ast: any): boolean {
 function generate_higher_order_function(ast: any[][]): string {
     let [head, ...tail] = ast;
     let result = "";
-    result += generate(ast[0]) + "(";
+    result += generate(head) + "(";
     for (let i = 0; i < tail.length; i++) {
         result += `${generate(tail[i])}`;
         if ((i + 1) < tail.length) {
