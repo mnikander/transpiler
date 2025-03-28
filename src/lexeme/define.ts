0f0e24de0acbc600d6e9da2135c2934e9f89e0ff
Marco Nikander <m4n1k.developer@gmail.com>
2025-03-28 15:58:49 +0100

    refactor 'define' to be strictly a binary function since 'declare' will be its own function

--- src/lexeme/define.ts
+++ src/lexeme/define.ts
@@ -3,6 +3,12 @@
 import assert from "assert";
 import { generate } from "../generate";
 
+export interface Define {
+    type: 'Define';
+    identifier: string; // TODO: change to Expression once everything is refactored
+    value: string;
+}
+
 export function is_define(ast: any): boolean {
     let [head, ...tail] = ast;
     return head == "define";
@@ -10,13 +16,10 @@ export function is_define(ast: any): boolean {
 
 export function generate_define(ast: any): string {
     let [head, ...tail] = ast;
-    if (tail.length == 1) {
-        return `auto const ${generate(tail[0])};`;
-    }
-    else if (tail.length == 2) {
+    if (tail.length == 2) {
         return `auto const ${generate(tail[0])} = ${generate(tail[1])};\n`;
     } else {
-        assert(false, `'define' requires 1 or 2 arguments, ${tail.length} provided <${tail.toString}>`);
+        assert(false, `'define' requires 2 arguments, ${tail.length} provided <${tail.toString}>`);
         return " /* ERROR: INCORRECT NUMBER OF ARGUMENTS */ ";
     }
 }
